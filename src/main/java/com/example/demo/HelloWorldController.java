0339add77e82c8c828057d5d197903d26a35e387
apavlov <pavlov.and.v@mail.ru>
2025-08-28 21:54:34 +0300

    Sugar measurement get, post and delete mapping

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,23 +1,30 @@
 package com.example.demo;
 
-import com.example.demo.dto.NameDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.IndexMeasurementDTO;
+import com.example.demo.dto.SugarMeasurementDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloWorldController {
-    public NameDTO nameDTO = new NameDTO();
+    public List<SugarMeasurementDTO> measurementDTOList = new ArrayList<>();
 
-    @GetMapping(path = "/hello")
-    public String helloWorld() {
-        return "hello world, " + nameDTO.name1;
+
+    @PostMapping(path = "/sugar/measurement")
+    public List<SugarMeasurementDTO> saveSugarMeasurement(@RequestBody SugarMeasurementDTO measurementDTO) {
+        measurementDTOList.add(measurementDTO);
+        return measurementDTOList;
     }
 
-    @PostMapping(path = "/name")
-    public NameDTO saveName(@RequestBody NameDTO name) {
-        this.nameDTO = name;
-        return this.nameDTO;
+    @GetMapping(path = "/sugar/measurement")
+    public List<SugarMeasurementDTO> getSugarMeasurements() {
+        return measurementDTOList;
+    }
+
+    @DeleteMapping(path = "/sugar/measurement")
+    public SugarMeasurementDTO deleteMeasurement(@RequestBody IndexMeasurementDTO indexDTO) {
+        return measurementDTOList.remove(indexDTO.index.intValue());
     }
 }
